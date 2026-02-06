67636fb0060113188eb6f703c071fae8fe99835a
Warner Zee <warner@zoynk.com>
2026-02-05 22:31:52 -0800

    Add fix for song titles starting with '.' as they may be interpreted as hidden files

--- ytmdl/dir.py
+++ ytmdl/dir.py
@@ -18,6 +18,7 @@ def __replace_special_characters(passed_name: str) -> str:
     / with a `-` so that it does not raise any errors
     related to the OS while moving the file
     """
+    passed_name = sub(r'^\.', '-', passed_name)
     return sub(r'[\\/|&?;:#~!"<>$%^*{}[\]+=`´]+', '-', passed_name)
 
 
